582310d7f5c5a33471600ed935504079d3610084
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-08-02 15:43:06 +0530

    added go routines and updated live calls and websocket

--- pkg/handlers/live.go
+++ pkg/handlers/live.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -29,27 +30,41 @@ func (s *StreamCamHandler) LivePage(c *gin.Context) {
 }
 
 func (s *StreamCamHandler) LiveStreamCam(c *gin.Context) {
+	c.Header("Content-Type", "multipart/x-mixed-replace; boundary=frame")
+
 	ws, err := websocket.Upgrade(c.Writer, c.Request, nil, 1024, 1024)
 	if err != nil {
 		log.Fatalf("Failed to upgrade WebSocket connection: %v", err)
 	}
-	stream, err := s.grpcClient.GetLiveStream(context.Background())
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	stream, err := s.grpcClient.GetLiveStream(ctx)
 	if err != nil {
 		log.Fatalf("Failed to call GetLiveStream: %v", err)
 	}
 
-	c.Header("Content-Type", "multipart/x-mixed-replace; boundary=frame")
+	go func() {
+		defer cancel()
 
-	for {
-		resp, err := stream.Recv()
-		if err == io.EOF {
-			break
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				fmt.Println("Error receiving streaming response:", err)
+				return
+			}
+			if err := ws.WriteMessage(websocket.BinaryMessage, resp.Data); err != nil {
+				fmt.Println("Failed to send video bytes over WebSocket:", err)
+				return
+			}
 		}
-		if err != nil {
-			log.Fatalf("Error receiving streaming response: %v", err)
-		}
-		if err := ws.WriteMessage(websocket.BinaryMessage, resp.Data); err != nil {
-			log.Fatalf("Failed to send video bytes over WebSocket: %v", err)
-		}
+	}()
+
+	_, _, err = ws.ReadMessage()
+	if err != nil && !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+		fmt.Println("Failed to read WebSocket message:", err)
 	}
 }
